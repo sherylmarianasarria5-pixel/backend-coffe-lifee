a87849f29650c1f5ba20636543dbd67e30954e6d
sherylmarianasarria5-pixel <sherylmarianasarria5@gmail.com>
2026-06-09 22:12:00 -0500

    fix: declarar idUsuario explicitamente en modelo Monitoreo

--- app/models/monitoreo.ts
+++ app/models/monitoreo.ts
@@ -1,5 +1,5 @@
 import { MonitoreoSchema } from '#database/schema'
-import { belongsTo, hasMany } from '@adonisjs/lucid/orm'
+import { belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
 import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
 
 import Cultivo from '#models/cultivo'
@@ -9,6 +9,10 @@ import AnalisisIa from '#models/analisis_ia'
 import Recomendacione from '#models/recomendacione'
 
 export default class Monitoreo extends MonitoreoSchema {
+
+  @column()
+  declare idUsuario: number | null
+
   @belongsTo(() => Cultivo, {
     foreignKey: 'idCultivo',
   })
